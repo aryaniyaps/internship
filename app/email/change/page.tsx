6127e4fd49a6f26bc2765f1d227eed51143bc3ff
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2023-06-20 22:06:50 +0530

    get basic email change flow working

--- app/email/change/page.tsx
+++ app/email/change/page.tsx
@@ -1,34 +1,47 @@
 "use client";
 import { Database } from "@/lib/database.types";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function CompleteEmailChangePage() {
+  // TODO: DELETE/ UPDATE EXISTING OAUTH CONNECTIONS ON EMAIL CHANGE
   const router = useRouter();
-  const { type, access_token } = router.query;
-  console.log(type, access_token);
+  const searchParams = new URLSearchParams(location.hash.substring(1));
+
+  const access_token = searchParams.get("access_token");
+  const refresh_token = searchParams.get("refresh_token");
+  const type = searchParams.get("type");
+
   const supabase = createClientComponentClient<Database>();
 
   useEffect(() => {
-    if (type === "recovery" && access_token) {
-      // complete the email change
-      completeEmailChange(access_token);
+    async function completeEmailChange(
+      access_token: string,
+      refresh_token: string
+    ) {
+      const { error } = await supabase.auth.setSession({
+        access_token,
+        refresh_token,
+      });
+      if (error) {
+        console.log(error.message);
+      } else {
+        router.push("/settings/account");
+      }
     }
-  }, [type, access_token]);
 
-  const completeEmailChange = async (access_token: any) => {
-    // const { error } = await supabase.auth.updateUser({}, {});
-    // if (error) {
-    //   console.log(error.message);
-    // } else {
-    //   router.push("/");
-    // }
-  };
+    if (type === "email_change" && access_token && refresh_token) {
+      // complete the email change
+      completeEmailChange(access_token, refresh_token);
+    }
+  }, [type, access_token, refresh_token, router, supabase]);
 
   return (
-    <div>
-      <h1>Completing email change...</h1>
+    <div className="w-full h-full flex justify-center items-center">
+      <h1 className="underline text-gray-400">
+        completing email change, redirecting
+      </h1>
     </div>
   );
 }
--- middleware.ts
+++ middleware.ts
@@ -25,5 +25,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/dashboard", "/settings/:path*"],
+  matcher: ["/dashboard", "/settings/:path*", "/email/change"],
 };
